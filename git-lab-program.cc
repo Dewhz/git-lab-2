3f4aa16aa459d6e38c0592a233eee077a7aa8b07
Frank Drew Hawley <dh438016@ohio.edu>
2022-04-01 03:10:53 -0400

    adding c++ file

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+        File: git-lab-program.cc
+        Author: Frank Drew Hawley
+        Date: 1 April 2022
+        Description: Test file for the lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
